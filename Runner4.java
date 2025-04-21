a056d19724c8a4962027530d86cb0636d92ace18
Tish <poston_quatisha@students.columbusstate.edu>
2025-04-20 22:13:28 -0400

    Add files via upload

---
+++ Runner4.java
@@ -0,0 +1,7 @@
+public class Runner4 {
+    ///Instantiate a Towers object to swap the rings on 3 "towers"
+    public static void main(String[] args)
+    {
+        Towers towersOfHanoi = new Towers(3);
+    }
+}
